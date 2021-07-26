6db43f3e475c72266f5b159240ce9eba3dc4a5a7
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-07-26 08:47:18 +0600

    Add files via upload

--- anagram.cpp
+++ anagram.cpp
@@ -2,44 +2,25 @@
 using namespace std ;
 
 
-bool anagram(string a , string b )
+void passing_string( string a , string b )
 {
-
-
     int len_a = a.length() ;
     int len_b = b.length() ;
-
-    if( len_a != len_b )
-        return(false) ;
     sort(a.begin() , a.end());
     sort(b.begin() , b.end());
 
-    for(int i = 0 ; i < len_a ; ++i )
-        if( a[i] != b[i])
-            return(false) ;
-
-    return(true);
+    if(a == b )
+        cout<<"The both string are Anagram"<<endl;
+    else
+        cout<<"The both string are not Anagram"<<endl;
 }
 
 int main()
 {
-    string a  ;
-    string b  ;
-    cout<<"Please enter two string \n" ;
-    getline(cin , a );
-    getline(cin , b );
+    string a = "BTEB";
+    string b = "BUBT";
 
-
-
-
-    if(anagram(a,b))
-        cout<<"yes this is anagram\n";
-    else
-        cout<<"this is not anagram\n" ;
-
-
-
-
+    passing_string(a, b);
 
     return(0);
 }
